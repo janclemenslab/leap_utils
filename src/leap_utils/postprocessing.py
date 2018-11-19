78abfb4522a122f61a183a22a482640f52b60023
Jan Clemens <clemensjan@googlemail.com>
2018-11-19 18:26:57 +0100

    detect confmap using 2D max

--- src/leap_utils/postprocessing.py
+++ src/leap_utils/postprocessing.py
@@ -1,10 +1,36 @@
 import numpy as np
 
 
+def max2d(X: np.array) -> (tuple, float):
+    """Get position and value of array maximum."""
+    row, col = np.unravel_index(X.argmax(), X.shape)
+    return (row, col), X[row, col]
+
+
+def max_simple(confmap: np.array) -> np.array:
+    """Detect maxima in each layer of the confmap.
+
+    Arguments:
+        confmap = []
+    """
+    if confmap.ndim != 3:
+        raise ValueError(f'input should be 3D (width x height x layers) but is {confmap.shape}.')
+
+    nb_parts = confmap.shape[-1]
+    peak_loc = np.zeros((nb_parts, 2))
+    peak_amp = np.zeros((nb_parts, 1))
+    for part_idx in range(nb_parts):
+        peak_loc[part_idx, ...], peak_amp[part_idx, ...] = max2d(confmap[..., part_idx])
+    return peak_loc, peak_amp
+
+
 def process_confmaps_simple(confmaps: np.array) -> (np.array, np.array):
     """Simply take the max."""
-    positions = None
-    confidence = None
+    nb_maps, nb_parts = confmaps.shape[0], confmaps.shape[-1]
+    positions = np.zeros((nb_maps, nb_parts, 2))
+    confidence = np.zeros((nb_maps, nb_parts, 1))
+    for nb, confmap in enumerate(confmaps):
+        positions[nb, ...], confidence[nb, ...] = max_simple(confmap)
     return positions, confidence
 
 
---
+++ tests/test_postprocessing.py
@@ -0,0 +1,38 @@
+import logging
+import numpy as np
+
+# from . import temp_dir
+
+from leap_utils.postprocessing import max2d, max_simple, process_confmaps_simple
+
+
+def test_max2d():
+    X = np.zeros((10, 10))
+    X[4, 5] = 1
+    loc, amp = max2d(X)
+    assert amp == 1
+    assert loc == (4, 5)
+
+    X = np.zeros((10, 10))
+    # X[4, 5] = 1
+    loc, amp = max2d(X)
+    print(loc, amp)
+    assert amp == 0
+    assert loc == (0, 0)
+
+
+def test_max_simple():
+    X = np.zeros((10, 10, 4))
+    X[2, 4, 0] = 2
+    positions, confidence = max_simple(X)
+    print(positions, confidence)
+    assert positions[0, 0] == 2 and positions[0, 1] == 4
+    assert confidence[0] == 2
+
+
+def test_process_confmaps_simple():
+    X = np.zeros((4, 10, 10, 4))
+    X[0, 2, 4, 0] = 2
+    positions, confidence = process_confmaps_simple(X)
+    assert positions.shape == (4, 4, 2)
+    assert confidence.shape == (4, 4, 1)
