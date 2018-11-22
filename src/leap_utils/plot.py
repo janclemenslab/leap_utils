2706b8f52844f072a4941f3e8917efa2c732d149
adpala93@gmail.com <43853502+adpala@users.noreply.github.com>
2018-11-22 14:42:37 +0100

    vplay tool incorporated in plot.py
    still missing extended description of usage

--- src/leap_utils/plot.py
+++ src/leap_utils/plot.py
@@ -44,3 +44,58 @@ def joint_distributions(positions, type):
     # https://seaborn.pydata.org/examples/multiple_joint_kde.html
     # https://seaborn.pydata.org/examples/cubehelix_palette.html
     pass
+
+
+def vplay(frames: np.array, idx: np.array=None, moviemode: bool=False):
+    """Plots boxes, either in a movie (moviemode = True) or frame by frame (moviemode = False)
+
+    Input: list of frames (output from export_boxes)
+
+        TODO: description of function and input.
+
+    """
+    import cv2
+
+    if idx is None:
+        idx = range(len(frames))
+
+    if len(idx) == len(frames)/2:
+        ridx = np.zeros(len(frames), dtype=int)
+        ridx[::2], ridx[1::2] = idx, idx
+        idx = ridx
+
+    if moviemode:
+        ii = 0
+        while True:
+            frame = frames[ii,...]
+            cv2.putText(frame, str(idx[ii]), (12,12), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0,0,250), lineType=4)
+            cv2.imshow('movie',frame)
+            ii += 1
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+
+            if ii >= len(frames)-1:
+                ii = 0
+
+    else:
+        ii = 0
+        while True:
+            frame = frames[ii,...]
+            cv2.putText(frame, str(idx[ii]), (12,12), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0,0,250), lineType=4)
+            cv2.imshow('movie',frame)
+            wkey = cv2.waitKey(0)
+
+            if wkey & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+            elif wkey & 0xFF == ord('d'):
+                ii += 1
+            elif wkey & 0xFF == ord('a'):
+                ii -= 1
+
+            if ii > len(frames)-1:
+                ii = 0
+            elif ii < 0:
+                ii = len(frames)-1
